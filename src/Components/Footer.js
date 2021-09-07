ac7b9d454d3d824e77f1ee1342a92f259a7b1c32
feras98nawafleh <feras.a.nawafleh@gmail.com>
2021-09-07 18:41:17 +0300

    final commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import { React, Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return <p>All Rights Reserved: Feras Nawafleh&copy;</p>
+  }
+}
+
+export default Footer;
