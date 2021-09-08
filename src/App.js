ca0cba59068b8f09774e0c4c4830dca8d7f12f75
feras98nawafleh <feras.a.nawafleh@gmail.com>
2021-09-08 21:37:10 +0300

    added likes lab02

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import { React, Component } from "react";
 import Header from './Components/Header';
 import Main from './Components/Main';
 import Footer from './Components/Footer';
+import data from './data.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animalData: data,
+    }
+  }
   render() {
     return (
       <>
         <Header />
-        <Main/>
+        <Main animalData={this.state.animalData}/>
         <Footer />
       </>
     )
